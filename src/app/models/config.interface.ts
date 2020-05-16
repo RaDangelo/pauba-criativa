7337f9a454df8998b1b06ce7148ce08b2d583aac
RaDangelo <rberga95@gmail.com>
2020-05-16 14:18:36 -0300

    initial project

---
+++ src/app/models/config.interface.ts
@@ -0,0 +1,13 @@
+export interface Configuration {
+    title: string,
+    name: string,
+    surname: string,
+    creationYear: string,
+    link1: string,
+    link2: string,
+    email: string,
+    phoneNumber: string,
+    instagram: string,
+    behanceUrl: string,
+    formUrl: string
+}
